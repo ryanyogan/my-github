1ee91e7ba0c198b453579eb88bb950fc20fad29b
Ryan Yogan <ryanyogan@gmail.com>
2018-09-06 12:27:40 -0700

    added basic profile/repo list w/ gql query

---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import Profile from '../Profile';
+
+class App extends Component {
+  state = {};
+  render() {
+    return <Profile />;
+  }
+}
+
+export default App;
